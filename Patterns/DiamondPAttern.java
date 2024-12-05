2e86419b90b60ccc8a3f164593af3a024b955a34
sarthak-chauhan77 <sarthakc981@gmail.com>
2024-12-05 20:09:14 +0530

    Update DiamondPAttern.java

--- Patterns/DiamondPAttern.java
+++ Patterns/DiamondPAttern.java
@@ -4,7 +4,7 @@ public class DiamondPAttern {
     DiamondPAttern() {
         int rows = 5;
 
-        // Upper part of the pattern (pyramid)
+        // Upper part of the pattern 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= rows - i; j++) { // Adjust spaces for correct alignment
                 System.out.print(" ");
@@ -15,7 +15,7 @@ public class DiamondPAttern {
             System.out.println();
         }
 
-        // Lower part of the pattern (inverted pyramid)
+        // Lower part of the pattern 
         for (int i = rows - 1; i >= 1; i--) {
             for (int j = 1; j <= rows - i; j++) { 
                 System.out.print(" ");
