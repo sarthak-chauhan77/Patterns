196220dcfee07694ca54a72d4570daae2c0f5c21
sarthak-chauhan77 <sarthakc981@gmail.com>
2024-12-08 15:24:53 +0530

    Add files via upload

---
+++ Patterns/TrianleStarPattern.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+/*
+      *
+     * *
+    * * *
+   * * * * 
+  * * * * * 
+ */
+public class TrianleStarPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
